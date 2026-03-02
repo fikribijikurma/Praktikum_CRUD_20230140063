2b81a1665e9c7d576e5770826f6ab5d783450a89
fikribijikurma <auliyafikrirahmadani@gmail.com>
2026-03-02 11:21:51 +0700

    Perbaikan validator.validate(request) pada Component Util

--- src/main/java/com/example/praktikum1/util/ValidationUtil.java
+++ src/main/java/com/example/praktikum1/util/ValidationUtil.java
@@ -2,7 +2,7 @@ package com.example.praktikum1.util;
 
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
-import javax.xml.validation.Validator;
+import jakarta.validation.Validator; // PERBAIKAN: Gunakan jakarta.validation
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,12 +12,14 @@ import java.util.Set;
 @Component
 @AllArgsConstructor
 public class ValidationUtil {
+
     @Autowired
     private Validator validator;
 
-    public void validate(Object request){
+    public void validate(Object request) {
+        // Sekarang method validate(Object) akan tersedia
         Set<ConstraintViolation<Object>> constraintViolations = validator.validate(request);
-        if (constraintViolations.size() !=0) {
+        if (!constraintViolations.isEmpty()) { // Menggunakan !isEmpty() lebih rapi
             throw new ConstraintViolationException(constraintViolations);
         }
     }
